94cd62f02edbeee6dea8d5f1f0b7d4333188cb43
herrhermon <hermon.almeida.outlook.com>
2022-10-12 17:23:24 -0300

    Primeiro passo - Limpar o React padrão e criar as seções do site em jsx simples no componente app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+// import React,{Fragment} from 'react';
+import React from 'react'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // <Fragment>
+    //Tem o mesmo valor de fragment
+    <>
+    <header className="topo"> Topo</header>
+    <main className ="principal">Principal</main>
+    <footer className="rodape">Rodapé</footer>
+    </>
+
+    // </Fragment>
   );
 }
 
